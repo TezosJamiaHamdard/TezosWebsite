740bed4f8814265cedc650e1e70bb6a7e9163e5a
Tezos JH <tezosjh@gmail.com>
2024-08-22 11:46:31 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,12 @@ import Page2 from './components/SecPage/PageSec'
 import Page3 from './components/ThirdPage/Page3'
 import Pagefour from './components/FourPage/Pagefour'
 import Footer from './components/footer/Footer'
-// import { Route , Routes } from "react-router-dom"
-// import About from './components/about/About'
-// import Blog from './components/blogs/Blog'
 
 
 const App = () => {
   return (
     <div>
       <Navbar/>
-      {/* <Routes>
-        <Route path='/about' element={<About/>} />
-        <Route path='/blogs' element={<Blog/>} />
-      </Routes> */}
       <Page1/>
       <Page3/>
       <Page2/>
